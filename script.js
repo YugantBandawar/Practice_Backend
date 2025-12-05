235a3853ae83d2891d244db830f64221e7c6954f
YugantBandawar <137055727+YugantBandawar@users.noreply.github.com>
2025-12-05 14:30:33 +0530

    Expressjs,Middleware,Routing

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-e
+import express from 'express'
+
+const app = express();
+
+app.use(function(req,res,next){
+    console.log("middleware run");
+    next();
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+app.get('/profile', (req, res) => {
+  //res.send('profile page hai ye');
+  return next(new Error("Not implemented"))
+})
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack)
+    res.status(500).send('Something broke')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
