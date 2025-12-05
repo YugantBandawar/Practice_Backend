136e21fa8533f53e5df7261bc440601c40e11a6f
YugantBandawar <137055727+YugantBandawar@users.noreply.github.com>
2025-12-05 14:51:15 +0530

    Session Cookie Parser

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ import express from 'express'
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
 app.use(function(req,res,next){
     console.log("middleware run");
     next();
